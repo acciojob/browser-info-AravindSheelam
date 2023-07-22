b184fee5385d122c16f0c007622c1bcbedb77556
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-22 19:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+console.log(navigator)
+let x= document.getElementById('browser-info')
+console.log(x)
+
+let browserName = navigator.appName
+let version = navigator.appVersion
+let y= "You are using " + browserName + " version " + version
+console.log(y)
+
+x.innerHTML = y
